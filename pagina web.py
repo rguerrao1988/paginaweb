cc2af7a2aaae2748e91a691695a886172ff3ebef
ramon <ramonguerraospino@hotmail.es>
2024-08-15 22:53:18 -0500

    page

--- pagina web.py
+++ pagina web.py
@@ -1,11 +1,11 @@
 #localhost:5000
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "home page"
+    return render_template("home.html")
 @app.route("/about")
 def about():
     return "about page"
