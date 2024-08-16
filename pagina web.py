a0cadae3b6475d3cd2486e3e8b45ed9b1e78d3e3
ramon <ramonguerraospino@hotmail.es>
2024-08-15 22:29:06 -0500

    page

--- pagina web.py
+++ pagina web.py
@@ -1,1 +1,15 @@
-#pagina web
+#localhost:5000
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "home page"
+@app.route("/about")
+def about():
+    return "about page"
+if __name__ == "__main__":
+    app.run()
+
+
---
+++ prueba.py
@@ -0,0 +1,15 @@
+'''
+import numpy as np
+numerosRandom = np.random.rand(5)
+d=numerosRandom[2]
+print(d)
+print(numerosRandom)
+'''
+import numpy as np
+import pandas as pd
+x = np.arange(15, dtype=np.int64).reshape(3, 5)
+x[1:, ::2] = -99
+'''
+import pandas as pd
+pd.test()
+'''
